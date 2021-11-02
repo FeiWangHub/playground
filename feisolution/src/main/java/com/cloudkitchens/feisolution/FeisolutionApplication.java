5da2caf278d2c91e998fba1b2df13740a9c49f6a
Fei <fei.wang.cn@icloud.com>
2021-11-02 22:33:48 +0800

    css interview init

---
+++ feisolution/src/main/java/com/cloudkitchens/feisolution/FeisolutionApplication.java
@@ -0,0 +1,13 @@
+package com.cloudkitchens.feisolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeisolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeisolutionApplication.class, args);
+	}
+
+}
---
+++ feisolution/src/test/java/com/cloudkitchens/feisolution/FeisolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudkitchens.feisolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeisolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
