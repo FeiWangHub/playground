16b7ead4eecb9e25747fc94f36d7bd1a9bf7ab64
Fei Wang <99712213+fei-tripalink@users.noreply.github.com>
2022-08-03 12:50:34 +0800

    Aug 3 : E findRepeat in array

--- src/main/java/com/fei/playground/algorithm/leetcode/E_Map_ContainsDuplicates_217.java
+++ src/main/java/com/fei/playground/algorithm/leetcode/E_Map_ContainsDuplicates_217.java
@@ -39,4 +39,26 @@ public class E_Map_ContainsDuplicates_217 {
         return false;
     }
 
+    /**
+     * 剑指 Offer 03. 数组中重复的数字
+     * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+     * https://leetcode.cn/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+     * <p>
+     * 原地交换法 每个数字，和每个数字index，是一一对应的 100% 84%
+     */
+    public int findRepeatNum(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] == i) {
+                i++;
+                continue;
+            }
+            if (nums[nums[i]] == nums[i]) return nums[i];
+            int tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+        }
+        return -1;
+    }
+
 }
