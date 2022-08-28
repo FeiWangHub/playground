f7618de936fd7e2e461604b9b8fd9c83b682d79b
Fei Wang <99712213+fei-tripalink@users.noreply.github.com>
2022-08-28 16:53:44 +0800

    math random util

--- feisolution/src/main/java/com/cloudkitchens/feisolution/util/MathUtil.java
+++ feisolution/src/main/java/com/cloudkitchens/feisolution/util/MathUtil.java
@@ -12,6 +12,11 @@ public class MathUtil {
         return rand.nextInt(max-min+1)+min;
     }
 
+    public static int getRandom(int max){
+        Random rand = new Random();
+        return rand.nextInt(max);
+    }
+
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
             System.out.println(getRandomWithinRange(3, 15));
