0d760e1087f93a09099c9bf1f6979e4eee7d945f
Fei Wang <99712213+fei-tripalink@users.noreply.github.com>
2022-09-06 21:21:58 +0800

    add gson for api test

--- src/main/java/com/fei/playground/algorithm/hackerRank/M_McKinsey_OA/M_RestAPI_AuthorArticles.java
+++ src/main/java/com/fei/playground/algorithm/hackerRank/M_McKinsey_OA/M_RestAPI_AuthorArticles.java
@@ -3,6 +3,10 @@ package com.fei.playground.algorithm.hackerRank.M_McKinsey_OA;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -37,17 +41,17 @@ import java.util.List;
  * • If both title and story_title are null, ignore the article.
  * 4. Based on the total pages count, fetch all the data (pagination) and perform step 3 for each.
  * 5. Return the array of titles.
- * 
+ * <p>
  * The query response from the website is a JSON response with the following five fields:
  * • page: The current page.
  * • per page: The maximum number of results per page.
  * • total. The total number of records in the search result.
  * • total pages: The total number of pages that must be queried to get all the results.
  * • data: An array of JSON objects containing article information
- * 
+ * <p>
  * Function Description
  * Complete the function getArticleTitles in the editor below.
- * 
+ * <p>
  * getArticleTitles has the following parameter(s):
  * string author: the author string to search on
  * Returns:
@@ -60,25 +64,25 @@ import java.util.List;
  */
 public class M_RestAPI_AuthorArticles {
 
-    public static List<String> getAuthorArticles(String author) {
+    public static List<String> getAuthorArticlesByFastJSON(String author) {
         LinkedList<String> result = new LinkedList<>();
         int curPage = 1;
         int totalPages = 1;
         String url = "https://jsonmock.hackerrank.com/api/articles?author=epaga&page=%s";
-        while(curPage <= totalPages){
+        while (curPage <= totalPages) {
             String respStr = httpGetRequest(String.format(url, curPage));
             JSONObject respJsonObj = (JSONObject) JSON.parse(respStr);
 
             totalPages = Integer.parseInt(respJsonObj.get("total_pages").toString());
             JSONArray jsonDataArray = respJsonObj.getJSONArray("data");
             Iterator<Object> it = jsonDataArray.iterator();
-            while (it.hasNext()){
+            while (it.hasNext()) {
                 JSONObject item = (JSONObject) it.next();
                 String title = (String) item.get("title");
                 String story_title = (String) item.get("story_title");
-                if(title != null){
+                if (title != null) {
                     result.add(title);
-                }else if(story_title != null){
+                } else if (story_title != null) {
                     result.add(story_title);
                 }
             }
@@ -89,7 +93,38 @@ public class M_RestAPI_AuthorArticles {
         return result;
     }
 
-    public static String httpGetRequest(String url){
+    public static List<String> getAuthorArticlesByGSON(String author) {
+        LinkedList<String> result = new LinkedList<>();
+        int curPage = 1;
+        int totalPages = 1;
+        String url = "https://jsonmock.hackerrank.com/api/articles?author=epaga&page=%s";
+
+        while (curPage <= totalPages) {
+            String respStr = httpGetRequest(String.format(url, curPage));
+
+            JsonObject respJsonObj = JsonParser.parseString(respStr).getAsJsonObject();
+            totalPages = respJsonObj.get("total_pages").getAsInt();
+
+            JsonArray jsonDataArray = respJsonObj.getAsJsonArray("data");
+            Iterator<JsonElement> it = jsonDataArray.iterator();
+            while (it.hasNext()) {
+                JsonObject item = it.next().getAsJsonObject();
+                String title = item.get("title").getAsString();
+                String story_title = item.get("story_title").getAsString();
+                if (title != null) {
+                    result.add(title);
+                } else if (story_title != null) {
+                    result.add(story_title);
+                }
+            }
+            curPage++;
+        }
+
+        System.out.println(result);
+        return result;
+    }
+
+    public static String httpGetRequest(String url) {
         System.out.println("!!!Debug URL: " + url);
         CloseableHttpClient httpClient = null;
         CloseableHttpResponse response = null;
@@ -123,7 +158,8 @@ public class M_RestAPI_AuthorArticles {
     }
 
     public static void main(String[] args) {
-        M_RestAPI_AuthorArticles.getAuthorArticles("epaga");
+//        M_RestAPI_AuthorArticles.getAuthorArticlesByFastJSON("epaga");
+        M_RestAPI_AuthorArticles.getAuthorArticlesByGSON("epaga");
     }
 
 }
