c8c961d593dce8d1f5bcebaafa89b9ad5e0b5364
Fei Wang <99712213+fei-tripalink@users.noreply.github.com>
2022-09-25 09:52:24 +0800

    JerryAI RangeList formatting

--- src/main/java/com/fei/playground/algorithm/company/jerryai/RangeList.java
+++ src/main/java/com/fei/playground/algorithm/company/jerryai/RangeList.java
@@ -18,34 +18,27 @@ public class RangeList {
     }
 
     /**
-     * TODO 入参效验
+     * Adds a range to the list
      *
-     * @param start inclusive
-     * @param end   exclusive
+     * @param start of range input, inclusive
+     * @param end   of range input, exclusive
      */
     public void add(int start, int end) {
         if (start >= end) return;
-        //5种情况
-        //1.独立新区间
-        //2.与前面区间相连
-        //3.与后边区间相连
-        //4.与前后都相连
-        //5.包含了一个或多个现有区间
 
         Map.Entry<Integer, Integer> leftRange = rangesMap.floorEntry(start);
         Map.Entry<Integer, Integer> rightRange = rangesMap.ceilingEntry(start);
-        System.out.println(String.format("rightRange: %s, leftRange: %s;", rightRange, leftRange));
 
-        //1. Range to add already contained in previous range
+        //1. if input range already contained in current rangesMap
         if (leftRange != null && leftRange.getKey() <= start && leftRange.getValue() >= end) return;
 
-        //2. Merge with left side range if mergable
+        //2. Merge with the closest left side range if mergable
         if (leftRange != null && start <= leftRange.getValue()) {
             start = leftRange.getKey();
             rangesMap.remove(leftRange.getKey());
         }
 
-        //3.持续向后检查ceiling是否可以merge
+        //3. Keep merging the closest right side range if mergable
         while (rightRange != null && rightRange.getKey() <= end) {//有overlap
             end = Math.max(end, rightRange.getValue());
             rangesMap.remove(rightRange.getKey());
@@ -56,38 +49,35 @@ public class RangeList {
     }
 
     /**
-     * TODO 入参效验
+     * Removes a range from the list
      *
-     * @param start start of range, inclusive
-     * @param end   end of range, exclusive
+     * @param start of range input, inclusive
+     * @param end   of range input, exclusive
      */
     public void remove(int start, int end) {
         if (start >= end) return;
 
         Map.Entry<Integer, Integer> leftRange = rangesMap.floorEntry(start);
         Map.Entry<Integer, Integer> rightRange = rangesMap.ceilingEntry(start);
-        System.out.println(String.format("rightRange: %s, leftRange: %s;", rightRange, leftRange));
 
-        //1 一个区间被完整移除
-        //2 一个区间被后半部分、前半部分、中间部分被移除
-        //3 没有任何移除
+        //1. check if the closest left side range removable
         if (leftRange != null && start != leftRange.getKey() && start < leftRange.getValue()) {
             rangesMap.remove(leftRange.getKey());
             if (end < leftRange.getValue()) {
-                // 1 从中间 把一个区间瓜分两半
+                // 1.1 split the range into 2 parts
                 this.add(leftRange.getKey(), start);
                 this.add(end, leftRange.getValue());
             } else {
-                //2 左边部分删除尾部 || 完全删除
+                //1.2 completely remove the range, or partially remove the tail part
                 this.add(leftRange.getKey(), start);
             }
         }
 
-        //向右寻找
+        //2. keep checking if the closest right side range removable
         while (rightRange != null && rightRange.getKey() < end) {
-            rangesMap.remove(rightRange.getKey());// 3.1 右边完全删除
+            rangesMap.remove(rightRange.getKey());// 2.1 completely remove the range
             if (rightRange.getValue() > end) {
-                // 3.2 右边部分删除
+                // 2.2 partially remove the head of the range
                 this.add(end, rightRange.getValue());
                 break;
             }
@@ -95,45 +85,56 @@ public class RangeList {
         }
     }
 
+    /**
+     * Prints out the list of ranges in the range list
+     */
     public void print() {
         rangesMap.forEach((key, value) -> {
             System.out.printf("[%s,%s)%n", key, value);
         });
-        System.out.println("----End---");//TODO delete
     }
 
     public static void main(String[] args) {
         RangeList r = new RangeList();
         r.add(1, 5);
-        r.add(1, 5);
         r.print();
+        System.out.println("-------- End of add[1, 5) --------");
 
         r.add(10, 20);
         r.print();
+        System.out.println("-------- End of add[10, 20) --------");
 
         r.add(20, 20);
         r.print();
+        System.out.println("-------- End of add[20, 20) --------");
 
         r.add(20, 21);
         r.print();
+        System.out.println("-------- End of add[20, 21) --------");
 
         r.add(2, 4);
         r.print();
+        System.out.println("-------- End of add[2, 4) --------");
 
         r.add(3, 8);
         r.print();
+        System.out.println("-------- End of add[3, 8) --------");
 
         r.remove(10, 10);
         r.print();
+        System.out.println("-------- End of remove[10, 10) --------");
 
         r.remove(10, 11);
         r.print();
+        System.out.println("-------- End of remove[10, 11) --------");
 
         r.remove(15, 17);
         r.print();
+        System.out.println("-------- End of remove[15, 17) --------");
 
         r.remove(3, 19);
         r.print();
+        System.out.println("-------- End of remove[3, 19) --------");
     }
 
 }
