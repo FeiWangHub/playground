29e3edede7aa980e2947cbe62291354dddb1643c
Fei Wang <99712213+fei-tripalink@users.noreply.github.com>
2022-09-29 09:25:50 +0800

    Thread Pool and Future

--- src/main/java/com/fei/playground/algorithm/MultiThread/ThreadPoolAndFutureTest.java
+++ src/main/java/com/fei/playground/algorithm/MultiThread/ThreadPoolAndFutureTest.java
@@ -3,14 +3,64 @@ package com.fei.playground.algorithm.MultiThread;
 import java.util.concurrent.*;
 import java.util.function.Supplier;
 
+/**
+ * Future没有返回值
+ * CompletableFuture有返回值
+ * (thenApplyAsync串行化处理另一个CompletableFuture)
+ * (anyOf()和allOf()用于并行化多个CompletableFuture)
+ *
+ * 廖雪峰教程 https://www.liaoxuefeng.com/wiki/1252599548343744/1306581182447650
+ */
 public class ThreadPoolAndFutureTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 //        testFuture();
-        testCompletableFuture();
+//        testCompletableFuture();
+        testSimpleCompletableFuture();
     }
 
-    public static void testFuture(){
+    //串行执行2个sync测试
+    public static void testSimpleCompletableFuture() throws InterruptedException, ExecutionException {
+        // 第一个任务:
+        CompletableFuture<String> cfQuery = CompletableFuture.supplyAsync(() -> {
+            return queryCode("中国石油");
+        });
+
+        cfQuery.thenAccept((e) -> System.out.println("e:" + e));
+//        cfQuery.get();
+
+        // cfQuery成功后继续执行下一个任务: (thenApplyAsync串行化处理另一个CompletableFuture)
+        CompletableFuture<Double> cfFetch = cfQuery.thenApplyAsync((code) -> {
+            return fetchPrice(code);
+        });
+        // cfFetch成功后打印结果:
+        cfFetch.thenAccept((result) -> {
+            System.out.println("price: " + result);
+        });
+
+        cfFetch.get();
+        //System.out.println(cfQuery.get());
+        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+        Thread.sleep(2000);
+    }
+
+    static String queryCode(String name) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        return "601857";
+    }
+
+    static Double fetchPrice(String code) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        return 5 + Math.random() * 20;
+    }
+
+    public static void testFuture() {
         System.out.println("main函数开始执行");
 
         ExecutorService executor = Executors.newFixedThreadPool(1);
@@ -32,9 +82,11 @@ public class ThreadPoolAndFutureTest {
     }
 
     //使用原生的CompletableFuture实现异步操作，加上对lambda的支持
-    public static void testCompletableFuture(){
+    public static void testCompletableFuture() {
         System.out.println("main函数开始执行");
         ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        //指定要异步执行的函数：get()
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(new Supplier<Integer>() {
             @Override
             public Integer get() {
@@ -48,7 +100,13 @@ public class ThreadPoolAndFutureTest {
                 return 3;
             }
         }, executor);
+        //如果执行成功，会执行下边的thenAccept
         future.thenAccept(e -> System.out.println(e));
+        //如果执行失败
+        future.exceptionally((e) -> {
+            e.printStackTrace();
+            return null;
+        });
         System.out.println("main函数执行结束");
     }
 
