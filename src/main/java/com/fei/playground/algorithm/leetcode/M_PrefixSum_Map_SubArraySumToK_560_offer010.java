1b47edfc82b7dad3ba48cadcb5e81657ef22764f
FeiWangHub <fei.wang.cn@icloud.com>
2022-10-08 16:34:35 +0800

    前缀和

--- src/main/java/com/fei/playground/algorithm/leetcode/M_PrefixSum_Map_SubArraySumToK_560_offer010.java
+++ src/main/java/com/fei/playground/algorithm/leetcode/M_PrefixSum_Map_SubArraySumToK_560_offer010.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
  * https://leetcode.cn/problems/subarray-sum-equals-k/
  * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
  */
-public class M_PrefixSum_Map_SubArray_To_K_560 {
+public class M_PrefixSum_Map_SubArraySumToK_560_offer010 {
 
     /**
      * 31.64% 49.63%
