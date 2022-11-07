afac568bc0c78f9e20157094da7e5623b7ceb62e
FeiWangHub <fei.wang.cn@icloud.com>
2022-11-07 18:55:23 +0800

    SlidingWindow check string inclusion

---
+++ src/main/java/com/fei/playground/algorithm/leetcode/M_DoublePointer_SlidingWindow_checkInclusion_567.java
@@ -0,0 +1,40 @@
+package com.fei.playground.algorithm.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 567. 字符串的排列
+ * 给你两个字符串s1和s2 ，写一个函数来判断 s2 是否包含 s1的排列。如果是，返回 true ；否则，返回 false 。
+ * 换句话说，s1 的排列之一是 s2 的 子串 。
+ * https://leetcode.cn/problems/permutation-in-string/
+ */
+public class M_DoublePointer_SlidingWindow_checkInclusion_567 {
+
+    /**
+     * 官方滑动窗口解法  79% 54%
+     */
+    public boolean checkInclusion_official(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < n; ++i) {
+            ++cnt1[s1.charAt(i) - 'a'];
+            ++cnt2[s2.charAt(i) - 'a'];
+        }
+        if (Arrays.equals(cnt1, cnt2)) {
+            return true;
+        }
+        for (int i = n; i < m; ++i) {
+            ++cnt2[s2.charAt(i) - 'a'];
+            --cnt2[s2.charAt(i - n) - 'a'];
+            if (Arrays.equals(cnt1, cnt2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
